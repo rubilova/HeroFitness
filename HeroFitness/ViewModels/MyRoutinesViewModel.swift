1f88cc3526cd4f4cbea0066e1ea5e8acb055360f
Elena Rubilova <yelena.rubilova@gmail.com>
2025-11-27 14:53:39 -0500

    ExploreView: show FilterChipsDemoView

--- HeroFitness/ViewModels/MyRoutinesViewModel.swift
+++ HeroFitness/ViewModels/MyRoutinesViewModel.swift
@@ -18,12 +18,7 @@ class MyRoutinesViewModel: ObservableObject {
     }
     
     func getRoutines() {
-        let routinesList = [
-            // running strength
-            Routine(id: "1", title: "Strength For Runners", thumbnail: "https://cdn.pixabay.com/photo/2016/11/19/12/21/man-1838991_1280.jpg", description: "Sets: 10", time: "30 minutes"),
-            Routine(id: "2", title: "Daily Mobility", thumbnail: "https://cdn.pixabay.com/photo/2022/10/11/10/06/woman-7513943_1280.jpg", description: "Sets: 8", time: "20 minutes"),
-            Routine(id: "3", title: "Dynamic Stretching", thumbnail: "https://cdn.pixabay.com/photo/2015/06/18/23/02/stretching-814227_1280.jpg", description: "Sets: 5", time: "10 minutes")
-        ]
+        let routinesList = mockRoutines
         routines = routinesList
     }
 }
